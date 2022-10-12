d635fe4ece670f71245edf1c276ad2a245be2835
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2022-10-12 17:59:37 +0800

    push new one

--- src/component/WeatherApp.js
+++ src/component/WeatherApp.js
@@ -1,34 +1,52 @@
-// import Dog from "../images/柯基.jpg";
+import Dog from "../images/柯基.jpg";
 // import { useState } from "react";
 import React, {
   useState
 } from 'react';
-const weather = () => {
-  // const [a,aaaaa]=useState(0)
-  const handleClick = () => {
-    fetch(
-      "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-8EE21382-44ED-437A-863F-0A251FC3BFB6"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('data', data);
-        const locationData = data.records.location[0];
-        const weatherElements = locationData.weatherElement.reduce(
-          (neededElements, item) => {
-            if (['WDSD', 'TEMP', 'HUMD'].includes(item.elementName)) {
-              neededElements[item.elementName] = item.elementValue;
-            }
-            return neededElements;
-          },
-          {})
-      });
-  };
-  // const [currentWeather, setCurrentWeather] = useState(1); 
+const WEATHER = () => {
+
+  const handleClick= async ()=>{
+    try {
+      const getUrl = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-8EE21382-44ED-437A-863F-0A251FC3BFB6`
+        // 取得遠端資源
+  const res = await fetch(`${getUrl}`)
+  // 使用 response.json() 將資源轉為 JSON 格式
+  const json = await res.json();
+  const locationData = json.records.location[0];
+  console.log(locationData);
+
+    } catch (error) {
+      console.log(error,"error")
+    }
+  }
+  // console.log(handleClick,"handleClick")
+  const [currentWeather, SetcurrentWeather] = useState({
+    time:"2022/10/11 週二 下午4:50",
+    local:"台北市",
+    description:"天氣乾冷",
+    temperature:21.5,
+    windSpeed:0.3,
+    humid:0.88
+  }); 
+
   return (
-    <div className="btn btn-danger" onClick={handleClick}>撈API資料</div>
+    <div>
+                      <div className="btn btn-danger" onClick={handleClick}>撈API資料</div>
+                      <div><img src={Dog} className='w-25'></img></div>
+              <div className="weather">
+            <main className="bg-white main">
+                <div className="city text-dark">城市:{currentWeather.local}</div>
+                <div className="city text-dark">日期:{currentWeather.time}</div>
+                <div className="status text-secondary">天氣狀況:{currentWeather.description}</div> 
+                <div className="temperature text-secondary">溫度:{currentWeather.temperature}<span>.c</span></div>
+                <div className="windSpeed text-secondary">風速:{currentWeather.windSpeed}m/h</div>
+                <div className="humidity text-secondary">濕度:{currentWeather.humid}</div>
+            </main>
+        </div>
+    </div>
     // <div className="bg-secondary weather">
     //     <div className="main">
-    //         <div className="text-dark fs-4">城市:{currentWeather.locationName}</div>
+    //         <div className="text-dark fs-4">城市:{CURRENTWEATHER.locationName}</div>
     //         <div>天氣描述:{currentWeather.description}</div>
     //         <div className="fs-1">溫度: {currentWeather.temperature}°C</div>
     //         <div>風速:{currentWeather.windSpeed}m/h</div>
@@ -39,4 +57,4 @@ const weather = () => {
     // </div>
   )
 }
-export default weather
+export default WEATHER
--- src/index.js
+++ src/index.js
@@ -9,12 +9,23 @@ import Head from "./component/head";
 // import ThreeIcon from "./component/threeSelectIcon"
 // import '../src';
 // import reportWebVitals from './reportWebVitals';
+function App() {
+  return (
+    <div>
+      <Head />
+      <WeatherApp />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-    <Head/>
-<WeatherApp/>
-  </div>
+root.render(<App />);
 
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <div>
+//     <Head/>
+// <WeatherApp/>
+//   </div>
+
+// );
