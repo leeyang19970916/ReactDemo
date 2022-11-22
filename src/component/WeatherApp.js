5f85e52622082743213c20ac4f32dc2d3b7e88b5
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2022-11-22 17:57:40 +0800

    1

--- src/component/WeatherApp.js
+++ src/component/WeatherApp.js
@@ -1,4 +1,4 @@
-import Dog from "../images/柯基.jpg";
+// import Dog from "../images/柯基.jpg";
 // import { ReactComponent as RainIcon } from '../images/柯基.jpg';
 // import { useState } from "react";
 
@@ -16,7 +16,19 @@ const WEATHER = () => {
     windSpeed: 0.3,
     humid: 0.88
   });
-  const handleClick = async () => {
+  useEffect(()=>{
+    const fetchData = async () => {
+      // STEP 2：使用 Promise.all 搭配 await 等待兩個 API 都取得回應後才繼續
+      const data = await Promise.all([
+        fetchCurrentWeather(),
+      ]);
+
+      console.log('data', data);
+    };
+    fetchData();
+  },[])
+
+  const fetchCurrentWeather = async () => {
     try {
       const getUrl = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-8EE21382-44ED-437A-863F-0A251FC3BFB6`
       // 取得遠端資源
@@ -35,28 +47,39 @@ const WEATHER = () => {
                 },
                 {},
               );
-      
+              console.log(res,"res")
               // STEP 3：要使用到 React 組件中的資料
-              setCurrentWeather({
-                observationTime: locationData.time.obsTime,
-                locationName: locationData.locationName,
-                description: '多雲時晴',
-                temperature: weatherElements.TEMP,
-                windSpeed: weatherElements.WDSD,
-                humid: weatherElements.HUMD,
-              });
+              // setCurrentWeather({
+                // observationTime: locationData.time.obsTime,
+                // locationName: locationData.locationName,
+                // description: '多雲時晴',
+                // temperature: weatherElements.TEMP,
+                // windSpeed: weatherElements.WDSD,
+                // humid: weatherElements.HUMD,
+              // });
+              // weatherElements.Wx.parameterName
+              setCurrentWeather((prevState => {
+                // 記得要回傳新的資料狀態回去
+                return {
+                  ...prevState,       
+                  observationTime: locationData.time.obsTime,
+                  locationName: locationData.locationName,
+                  description: '唉唉',
+                  temperature: weatherElements.TEMP,
+                  windSpeed: weatherElements.WDSD,
+                  humid: weatherElements.HUMD,     // 保留原有的資料狀態
+                  rainPossibility: 0.1    // 添加或更新的資料
+                }
+              }))
 
     } catch (error) {
       console.log(error, "error")
     }
   }
-  // console.log(handleClick,"handleClick")
-
-console.log(currentWeather,"currentWeather")
   return (
     <div>
-      <div className="btn btn-danger" onClick={handleClick}>撈API資料</div>
-      <div><img src={Dog} className='w-25'></img></div>
+      <div className="btn btn-danger" onClick={fetchCurrentWeather}>撈API資料</div>
+      {/* <div><img src={Dog} className='w-25'></img></div> */}
       <div className="weather">
         <main className="bg-white main">
           <div className="city text-dark">城市:{currentWeather.locationName}</div>
@@ -65,6 +88,7 @@ console.log(currentWeather,"currentWeather")
           <div className="temperature text-secondary">溫度:{currentWeather.temperature}<span>.c</span></div>
           <div className="windSpeed text-secondary">風速:{currentWeather.windSpeed}m/h</div>
           <div className="humidity text-secondary">濕度:{currentWeather.humid}</div>
+          <i className="fa-solid fa-house"></i>
         </main>
       </div>
     </div>
